1d3b0937f9d357fb691e66a9038354b8ad0db093
tom <tomhackbarth@gmail.com>
2022-01-10 19:09:44 -0500

    Added Details info

--- app/src/main/java/com/thackbarth/playwithjetpack/navigation/screens/ui/Details.kt
+++ app/src/main/java/com/thackbarth/playwithjetpack/navigation/screens/ui/Details.kt
@@ -1,16 +1,40 @@
 package com.thackbarth.playwithjetpack.navigation.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.thackbarth.playwithjetpack.model.MainViewModel
 import com.thackbarth.playwithjetpack.model.Photo
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
 
 @Composable
 fun DetailsScreen(navController: NavController, id: Int?){
 
     var photo = MainViewModel.photoListResponse.filter { it.id == id  }.first()
-    Text(text = "Details go here: ${photo.title}")
+
+    Column(
+        modifier = Modifier.fillMaxWidth().padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+        Row(modifier = Modifier.padding(20.dp)){
+            Image(
+                painter = rememberImagePainter(photo.url),
+                contentDescription = "${photo.title}"
+            )
+        }
+
+        Text(text = "Details go here: ${photo.title}")
+    }
+
+
 
 
 }
