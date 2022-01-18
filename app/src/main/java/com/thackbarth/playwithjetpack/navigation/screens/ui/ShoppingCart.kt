fe5facf5ba14c068dcca3e27c7fb9def5498fde8
tom <tomhackbarth@gmail.com>
2022-01-18 11:55:31 -0500

    WIP

--- app/src/main/java/com/thackbarth/playwithjetpack/navigation/screens/ui/ShoppingCart.kt
+++ app/src/main/java/com/thackbarth/playwithjetpack/navigation/screens/ui/ShoppingCart.kt
@@ -1,12 +1,18 @@
 package com.thackbarth.playwithjetpack.navigation.screens.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
+import com.thackbarth.playwithjetpack.composables.CartRow
+import com.thackbarth.playwithjetpack.model.CartItem
 import com.thackbarth.playwithjetpack.model.MainViewModel
+import com.thackbarth.playwithjetpack.navigation.screens.ApplicationScreens
 import kotlinx.coroutines.InternalCoroutinesApi
 
 @InternalCoroutinesApi
@@ -33,7 +39,34 @@ fun ShoppingCart(navController: NavController, mainViewModel: MainViewModel) {
         )
     },
         content = {
-            Text(text = "Shopping cart goes here")
+            ShoppingCartScreenContent( navController, mainViewModel)
         })
 
+}
+
+@InternalCoroutinesApi
+@Composable
+fun ShoppingCartScreenContent(navController: NavController, viewModel: MainViewModel) {
+
+    val lst = viewModel.shoppingCart.value?.toList()
+
+    Surface(color = Color.White) {
+        Column() {
+            if (lst != null) {
+                DisplayShoppingCartRow(navController = navController, lst)
+            }
+        }
+    }
+}
+
+@InternalCoroutinesApi
+@Composable
+fun DisplayShoppingCartRow(navController: NavController, lst: List<CartItem>) {
+    LazyColumn {
+        itemsIndexed(items = lst) { index, item ->
+            CartRow(item) {
+//                navController.navigate(route = ApplicationScreens.DetailsScreen.name + "/" + item.id)
+            }
+        }
+    }
 }
